d9d28b85d113fe809f6f8bcebb6706341c56514c
md-aswindas <117567492+md-aswindas@users.noreply.github.com>
2022-12-10 13:30:18 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,12 @@
+print("----SWAPPING OF TWO NUMBERS----")
+a=int(input("enter a number : "))
+b=int(input("enter another number : "))
+print("Before Swapping")
+print("a = ",a)
+print("b = ",b)
+temp=a
+a=b
+b=temp
+print("After Swapping")
+print("a = ",a)
+print("b = ",b)
